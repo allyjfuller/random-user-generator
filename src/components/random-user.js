b5b53939d41a310935dc9c7ee00ccb444ad1d7bf
allyjfuller <allyjfuller@gmail.com>
2019-03-20 18:23:08 -0400

    working perfectly no errors

--- src/components/random-user.js
+++ src/components/random-user.js
@@ -1,25 +1,24 @@
 import React from 'react';
 
 function RandomUser(props) {
-	console.log(props);
 	return (
 		<div>
 		<ul>
 			{props.user.map((user, index) => {
-				return(
-					<div>
-					<h1 key={user}>{user.name.first} {user.name.last}</h1>
-					<li>{user.phone}</li>
-					<li>{user.email}</li>
-					<li>{user.gender}</li>
-					<li>{user.location.city}</li>
+				return (
+					<div key={index}>
+					<h1>{user.name.first} {user.name.last}</h1>
+					<li>Phone: {user.phone}</li>
+					<li>Email: {user.email}</li>
+					<li>Gender: {user.gender}</li>
+					<li>Residence: {user.location.city}</li>
 					</div>
-				)
-			
+
+
+					)
 			})}
 
-
-		</ul>
+			</ul>
 		</div>
 
 	)
